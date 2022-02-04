e85be3ed9c0e37b5043004de41e03d2760496069
Begal Wadim <begal-vadim98@yandex.ru>
2022-02-04 10:42:59 +0300

    Залил файлы дз

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const guessNumber = function (attempts) {
+const guessNumber = function () {
 
   // Генерируем случайное число
   const getRandomInt = function (min, max) {
@@ -8,97 +8,95 @@ const guessNumber = function (attempts) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
   }
-//  Записываем результат в переменную в указанном интервале
 
+  //  Записываем результат в переменную в указанном интервале
   const resultGetRandomInit = getRandomInt(1, 100);
 
   // Основная частья
-  const baseGame = function () {
-    attempts--
-    console.log(attempts)
+  const baseGame = function (attempts) {
 
-    
-    // спрашиваем пользователя
+    // Уменьшаем колличество попыток
+    attempts--
+
+    // спрашиваем пользователя число
     const questionUser = function () {
 
-      let userNumber =  prompt("Угадай число от 1 до 100");
+      let userNumber = prompt("Угадай число от 1 до 100");
 
       return userNumber;
     }
-
+    
+    // Записываем результат в перемннную
     const resultFuncQuestionUser = questionUser();
 
-
-    console.log(resultGetRandomInit);
-
-    // Попытки
-    // const attemptsFunc = function () {
-
-    //   switch (true) {
-    //     case attempts > 0: 
-    //     baseGame();
-    //     break
-
-    //    case attempts = 0: 
-    //     baseGame().stop();
-    //     break
-    //   }
-      
-    // }
-    
-    // attemptsFunc();
-    // Уведомляем пользователя о результате
+    // Уведомляем пользователя о результате и производим дальнейшие действия
     const responseOutput = function () {
 
       switch (true) {
+        // В случае окончания попыток
+        case attempts === 0:
+          let resultAnswer = confirm("Попытки закончились, хотите сыграть еще?");
+          switch (resultAnswer) {
+            case false:
+              alert("Игра окончена");
+              break;
 
-      
+            case true:
+              guessNumber();
+              break;
 
+          }
+          break;
+
+         // Число было угаданно
+        case parseFloat(resultFuncQuestionUser) === resultGetRandomInit:
+          let answer = confirm("Поздравляю, Вы угадали!!!, Хотели бы поиграть еще??");
+
+          switch (answer) {
+
+            case false:
+              alert("Игра окончена");
+              break;
+
+            case true:
+              guessNumber();
+              break;
+
+          }
+
+          break;
+        
+        // Пользователь нажал отмена
         case !resultFuncQuestionUser:
-            alert("Отмена");
-            break
-
+          alert("Это ваше прощальное сообщение мы выключаемся");
+          break
+        
+        // Введенно не число
         case !parseFloat(resultFuncQuestionUser):
           alert("Введи число");
-          baseGame();
+          // Возвращаем колличество попыток на одну
+          baseGame(attempts + 1);
           break
 
 
 
         case resultFuncQuestionUser > resultGetRandomInit:
-          alert("Загаданное число меньше");
-          baseGame();
+          alert(`Загаданное число меньше осталось попыток ${attempts}`);
+          baseGame(attempts);
           break
 
         case resultFuncQuestionUser < resultGetRandomInit:
-          alert("Загаданное число больше"); 
-          baseGame();
+          alert(`Загаданное число больше осталось попыток ${attempts}`);
+          baseGame(attempts);
           break
-
-        case parseFloat(resultFuncQuestionUser) === resultGetRandomInit:
-          alert("Поздравляю, Вы угадали!!!");
-          break
-         
-          // case attempts > 0: 
-          // baseGame();
-          // break
-  
-         case attempts === 0: 
-         
-          alert("все")
-          return;
-          break
-
       }
 
     }
 
-    responseOutput(); 
+    responseOutput();
 
-   
   }
-
-  baseGame();
+  baseGame(10);
 }
 
-guessNumber(10);
+guessNumber();
